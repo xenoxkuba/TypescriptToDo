12b982c5738f92881ba8cbd07ae4d08f886d6d98
xenoxkuba <jakub.purczynski@onet.pl>
2023-05-06 13:11:33 +0200

    commit

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,32 @@
+import { useRef, useContext } from "react";
+
+import { TodosContext } from "../store/todos-context";
+
+const NewTodo: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
+
+    todosCtx.addTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Typescript ToDo</label>
+      <input type="text" id="text" ref={todoTextInputRef} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,7 @@
+const TodoItem: React.FC<{ text: string; onRemoveTodo: () => void }> = (
+  props
+) => {
+  return <li onClick={props.onRemoveTodo}>{props.text}</li>;
+};
+
+export default TodoItem;
